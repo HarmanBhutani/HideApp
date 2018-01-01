5adf38711c698683827eb58202894fa68c5c375c
Harman Bhutani <byteharman0101@gmail.com>
2018-01-01 21:11:52 +0530

    Initial Commit

---
+++ HideApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HideApp
+//
+//  Created by HARMAN BHUTANI on 01/01/18.
+//  Copyright © 2018 HARMAN BHUTANI. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ HideApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HideApp
+//
+//  Created by HARMAN BHUTANI on 01/01/18.
+//  Copyright © 2018 HARMAN BHUTANI. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
